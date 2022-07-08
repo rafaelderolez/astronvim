0c3c3c9f627066532eb45cfdcbdffffe03a2532f
Rafael Derolez <raf@derolez.dev>
2022-07-08 11:57:01 +0200

    Format plugins init

--- plugins/init.lua
+++ plugins/init.lua
@@ -2,15 +2,14 @@ return {
   {
     "catppuccin/nvim",
     as = "catppuccin",
-    config = function()
-      require("catppuccin").setup {}
-    end,
+    config = function() require("catppuccin").setup {} end,
   },
   "windwp/nvim-spectre",
   {
     "jose-elias-alvarez/typescript.nvim",
     after = "nvim-lsp-installer",
     config = function()
+      ---@diagnostic disable-next-line: different-requires
       require("typescript").setup { require "user.plugins.typescript" }
     end,
   },
