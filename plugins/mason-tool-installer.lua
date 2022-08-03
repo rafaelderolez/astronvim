3c853df80283be462fe7238b66a1f8e721c2d479
Rafael Derolez <raf@derolez.dev>
2022-08-03 15:26:39 +0200

    Add mason-tool-installer config

---
+++ plugins/mason-tool-installer.lua
@@ -0,0 +1,15 @@
+return {
+  ensure_installed = {
+    "stylua",
+    "actionlint",
+    "eslint_d",
+    "pylint",
+    "selene",
+    "yamllint",
+    "chrome-debug-adapter",
+    "firefox-debug-adapter",
+    "fixjson",
+    "prettierd",
+    "shfmt",
+  },
+}
