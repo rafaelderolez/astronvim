e629f1913eaa9d5d0a99f38f2f1e4e542f4f88d4
Rafael Derolez <raf@derolez.dev>
2022-08-03 15:26:46 +0200

    Add mason-lspconfig config

---
+++ plugins/mason-lspconfig.lua
@@ -0,0 +1,11 @@
+return {
+  ensure_installed = {
+    "css-lsp",
+    "graphql-language-service-cli",
+    "html-lsp",
+    "json-lsp",
+    "lua-language-server",
+    "typescript-language-server",
+    "yaml-language-server",
+  },
+}
