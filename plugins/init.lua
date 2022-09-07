d850c7925cee0f7059251901e292991abad88d96
Rafael Derolez <raf@derolez.dev>
2022-09-07 11:02:08 +0200

    DiffView

--- plugins/init.lua
+++ plugins/init.lua
@@ -2,14 +2,19 @@ return {
   {
     "catppuccin/nvim",
     as = "catppuccin",
-    config = function() require("catppuccin").setup {} end,
+    config = function() require("catppuccin").setup(require "user.plugins.catppuccin") end,
   },
-  "windwp/nvim-spectre",
-  "ggandor/lightspeed.nvim",
+  "windwp/nvim-spectre", -- find & replace
+  "ggandor/lightspeed.nvim", -- motion
   {
-    "nvim-treesitter/nvim-treesitter-context",
-    config = function() require("treesitter-context").setup { require "user.plugins.treesitter-context" } end,
+    "nvim-treesitter/nvim-treesitter-context", -- show current sticky context
+    config = function() require("treesitter-context").setup(require "user.plugins.treesitter-context") end,
   },
-  "vuki656/package-info.nvim",
+  -- "vuki656/package-info.nvim", -- package.json helper (doesn't really work)
+  {
+    "sindrets/diffview.nvim", -- git diff view
+    requires = "nvim-lua/plenary.nvim",
+    config = function() require("diffview").setup(require "user.plugins.diffview") end,
+  },
   -- "nvim-telescope/telescope-file-browser.nvim",
 }
