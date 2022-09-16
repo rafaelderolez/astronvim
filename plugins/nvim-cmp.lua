a4f349ca8b91cd81404599cdcb9b725cb78b9359
Rafael Derolez <raf@derolez.dev>
2022-09-16 16:27:11 +0200

    Copilot

--- plugins/nvim-cmp.lua
+++ plugins/nvim-cmp.lua
@@ -1,8 +1,16 @@
 return {
   source_priority = {
+    copilot = 1250,
     nvim_lsp = 1000,
     luasnip = 750,
     buffer = 500,
     path = 250,
+  },
+  formatting = {
+    format = astronvim.lspkind.cmp_format {
+      mode = "symbol",
+      max_width = 50,
+      symbol_map = { Copilot = "" },
+    },
   },
 }
