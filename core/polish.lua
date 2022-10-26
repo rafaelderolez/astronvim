fea04cefdea6215563e154d135b8bd485d16d416
Rafael Derolez <raf@derolez.dev>
2022-10-26 09:32:41 +0200

    Fix macos specific mappings

--- core/polish.lua
+++ core/polish.lua
@@ -3,11 +3,14 @@ return function()
 
   -- Move lines/selections up and down
   vim.cmd [[
-    noremap ∆ <Esc>:m .+1<CR>
-    noremap ˚ <Esc>:m .-2<CR>
-    vnoremap ∆ :m '>+1<CR>gv=gv
-    vnoremap ˚ :m '<-2<CR>gv=gv
+    nnoremap <A-j> :m .+1<CR>==
+    nnoremap <A-k> :m .-2<CR>==
+    inoremap <A-j> <Esc>:m .+1<CR>==gi
+    inoremap <A-k> <Esc>:m .-2<CR>==gi
+    vnoremap <A-j> :m '>+1<CR>gv=gv
+    vnoremap <A-k> :m '<-2<CR>gv=gv
   ]]
+
   -- Set autocommands
   vim.api.nvim_create_augroup("telescope", {})
   vim.api.nvim_create_autocmd("VimEnter,ColorScheme", {
