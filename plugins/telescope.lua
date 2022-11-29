8223fadf48e34269b3ec0911ff7ca522a35fdb45
Rafael Derolez <raf@derolez.dev>
2022-11-29 21:46:54 +0100

    Telescope config

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,4 +1,24 @@
 return {
+  defaults = {
+    layout_config = {
+      width = 0.90,
+      height = 0.85,
+      preview_cutoff = 120,
+      horizontal = {
+        preview_width = 0.6,
+      },
+      vertical = {
+        width = 0.9,
+        height = 0.95,
+        preview_height = 0.5,
+      },
+      flex = {
+        horizontal = {
+          preview_width = 0.9,
+        },
+      },
+    },
+  },
   extensions = {
     project = {
       base_dirs = {
