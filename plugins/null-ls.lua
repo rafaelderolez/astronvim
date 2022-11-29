6edcd0a4455c7d981457bda9f6ce54e82e4c082b
Rafael Derolez <raf@derolez.dev>
2022-11-29 10:43:22 +0100

    Clean up null-ls

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,11 +1,10 @@
 local status_ok, null_ls = pcall(require, "null-ls")
 
-local command_resolver = require "null-ls.helpers.command_resolver"
 local formatting = null_ls.builtins.formatting
 
 -- Check supported linters
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-local diagnostics = null_ls.builtins.diagnostics
+-- local diagnostics = null_ls.builtins.diagnostics
 
 -- Check supported code_actions
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
@@ -17,31 +16,10 @@ local codeActions = null_ls.builtins.code_actions
 -- Check supported formatters
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 
-if status_ok then
-  return {
-    debug = false,
-    sources = {
-      -- Formatting
-      formatting.stylua,
-      formatting.eslint_d.with {
-        dynamic_command = command_resolver.from_node_modules(),
-      },
-
-      -- formatting.prettierd.with {
-      --   dynamic_command = command_resolver.from_node_modules(),
-      --   env = {
-      --     PRETTIERD_LOCAL_PRETTIER_ONLY = true,
-      --   },
-      -- },
-
-      -- Diagnostics
-      diagnostics.eslint_d.with {
-        dynamic_command = command_resolver.from_node_modules(),
-      },
-
-      codeActions.eslint_d.with {
-        dynamic_command = command_resolver.from_node_modules(),
-      },
-    },
-  }
-end
+if status_ok then return {
+  debug = false,
+  sources = {
+    formatting.stylua,
+    codeActions.eslint_d,
+  },
+} end
