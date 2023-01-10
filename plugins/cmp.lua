237c63dc672e9a558ee3ade11a21753ece9da609
Rafael Derolez <raf@derolez.dev>
2023-01-10 12:11:36 +0100

    Round cmp window

---
+++ plugins/cmp.lua
@@ -0,0 +1,10 @@
+return {
+  window = {
+    completion = {
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    },
+    documentation = {
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    },
+  },
+}
