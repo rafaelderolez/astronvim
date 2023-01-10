cddae6eebc9728c82eb62818f1578cf1388a318f
Rafael Derolez <raf@derolez.dev>
2023-01-10 12:11:44 +0100

    Fix diffview settings

--- plugins/diffview.lua
+++ plugins/diffview.lua
@@ -52,12 +52,15 @@ return {
     },
   },
   file_history_panel = {
-    log_options = { -- See ':h diffview-config-log_options'
-      single_file = {
-        diff_merges = "combined",
-      },
-      multi_file = {
-        diff_merges = "first-parent",
+    log_options = {
+      git = {
+        single_file = {
+          max_count = 512,
+          follow = true,
+        },
+        multi_file = {
+          max_count = 128,
+        },
       },
     },
     win_config = { -- See ':h diffview-config-win_config'
