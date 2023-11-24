3262a322a411c7438ede2f6ee2cd2de183b232bf
Rafael Derolez <raf@derolez.dev>
2023-11-24 13:25:26 +0100

    Set opts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,9 @@
 -- Default options that are always set: https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/options.lua
 -- Add any additional options here
 
--- vim.opt.relativenumber = true -- sets vim.opt.relativenumber
--- vim.opt.number = true -- sets vim.opt.number
--- vim.opt.spell = false -- sets vim.opt.spell
--- vim.opt.signcolumn = "auto" -- sets vim.opt.signcolumn to auto
--- vim.opt.wrap = false -- sets vim.opt.wrap
+vim.opt.relativenumber = true -- sets vim.opt.relativenumber
+vim.opt.spell = false -- sets vim.opt.spell
+vim.opt.signcolumn = "auto" -- sets vim.opt.signcolumn to auto
+vim.opt.wrap = true -- sets vim.opt.wrap
 
 -- vim.g.mapleader = " " -- sets vim.g.mapleader
