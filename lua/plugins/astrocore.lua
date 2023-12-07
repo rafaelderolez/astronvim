6934888ce2687df65d644e1f80876dc0ac603199
Rafael Derolez <raf@derolez.dev>
2023-12-07 08:27:49 +0100

    Move astrocode into own file

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -20,6 +20,14 @@ local opts = {
       },
     },
   },
+  -- modify core features of AstroNvim
+  features = {
+    max_file = { size = 1024 * 100, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+    autopairs = true, -- enable autopairs at start
+    cmp = true, -- enable completion at start
+    highlighturl = true, -- highlight URLs at start
+    notifications = true, -- enable notifications at start
+  },
 }
 
 return { "AstroNvim/astrocore", opts = opts }
