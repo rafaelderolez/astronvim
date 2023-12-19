37307411891a8d2a172c0225784414508daa0140
Rafael Derolez <raf@derolez.dev>
2023-12-19 23:18:49 +0100

    Clean up luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,16 +1,13 @@
-return {}
-
--- return {
---   "L3MON4D3/LuaSnip",
---   config = function(plugin, opts)
---     -- include the default astronvim config that calls the setup call
---     require "plugins.configs.luasnip"(plugin, opts)
---     -- load snippets paths
---     require("luasnip.loaders.from_vscode").lazy_load {
---       -- this can be used if your configuration lives in ~/.config/nvim
---       -- if your configuration lives in ~/.config/astronvim, the full path
---       -- must be specified in the next line
---       paths = { "./lua/user/snippets" },
---     }
---   end,
--- }
+return {
+  "L3MON4D3/LuaSnip",
+  dependencies = { "rafamadriz/friendly-snippets" },
+  -- follow latest release.
+  version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  -- install jsregexp (optional!).
+  build = "make install_jsregexp",
+  config = function(plugin, opts)
+    require("luasnip.loaders.from_vscode").lazy_load {
+      paths = { "./lua/snippets" },
+    }
+  end,
+}
