fbb215c195241efbe25d6c33be3bafe58e68daf6
Rafael Derolez <raf@derolez.dev>
2024-03-11 22:52:20 +0100

    Migrate autocmds to astrocore

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,33 +1,13 @@
 ---@type AstroCoreOpts
-local opts = {
-  autocmds = {
-    disabled_comment_continuation = {
-      {
-        event = "FileType",
-        desc = "Disable comment continuation",
-        command = "set formatoptions-=cro",
-      },
+return {
+  "AstroNvim/astrocore",
+  opts = {
+    features = {
+      max_file = { size = 1024 * 100, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      autopairs = true, -- enable autopairs at start
+      cmp = true, -- enable completion at start
+      highlighturl = true, -- highlight URLs at start
+      notifications = true, -- enable notifications at start
     },
-    auto_spell = {
-      {
-        event = "FileType",
-        desc = "Enable wrap and spell for text like documents",
-        pattern = { "gitcommit", "markdown", "text", "plaintex" },
-        callback = function()
-          vim.opt_local.wrap = true
-          vim.opt_local.spell = true
-        end,
-      },
-    },
-  },
-  -- modify core features of AstroNvim
-  features = {
-    max_file = { size = 1024 * 100, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-    autopairs = true, -- enable autopairs at start
-    cmp = true, -- enable completion at start
-    highlighturl = true, -- highlight URLs at start
-    notifications = true, -- enable notifications at start
   },
 }
-
-return { "AstroNvim/astrocore", opts = opts }
