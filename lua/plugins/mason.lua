2a54e3959db97b4317889ce853114cfbaaa55b9a
Rafael Derolez <raf@derolez.dev>
2024-05-28 09:22:02 +0200

    Update mason settings

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Example customization of mason plugins
 ---@type LazySpec
@@ -16,31 +16,31 @@ return {
   --   end,
   -- },
   -- -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "biome" })
-
-      opts.handlers.biome = function()
-        local null_ls = require "null-ls"
-        local has_biome = function(util) return util.root_has_file "biome.json" end
-
-        null_ls.register(null_ls.builtins.formatting.biome.with {
-          condition = has_biome,
-          args = {
-            "check",
-            "--apply-unsafe",
-            "--formatter-enabled=true",
-            "--organize-imports-enabled=true",
-            "--skip-errors",
-            "$FILENAME",
-          },
-        })
-      end
-    end,
-  },
+  -- {
+  --   "jay-babu/mason-null-ls.nvim",
+  --   -- overrides `require("mason-null-ls").setup(...)`
+  --   opts = function(_, opts)
+  --     -- add more things to the ensure_installed table protecting against community packs modifying it
+  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "biome" })
+  --
+  --     opts.handlers.biome = function()
+  --       local null_ls = require "null-ls"
+  --       local has_biome = function(util) return util.root_has_file "biome.json" end
+  --
+  --       null_ls.register(null_ls.builtins.formatting.biome.with {
+  --         condition = has_biome,
+  --         args = {
+  --           "check",
+  --           "--apply-unsafe",
+  --           "--formatter-enabled=true",
+  --           "--organize-imports-enabled=true",
+  --           "--skip-errors",
+  --           "$FILENAME",
+  --         },
+  --       })
+  --     end
+  --   end,
+  -- },
   -- {
   --   "jay-babu/mason-nvim-dap.nvim",
   --   -- overrides `require("mason-nvim-dap").setup(...)`
