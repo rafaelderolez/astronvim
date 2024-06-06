82c3ad5db770613244d4512211c5a16e44086049
Rafael Derolez <raf@derolez.dev>
2024-06-06 23:42:56 +0200

    Disable lua_ls formatting
    Use stylua instead

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -29,7 +29,7 @@ return {
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
+        "lua_ls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
