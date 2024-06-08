a58ddc48fac28bf23b4b62feb5153453ea9097be
Rafael Derolez <raf@derolez.dev>
2024-06-08 22:03:03 +0200

    Change to tokyonight-storm

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,6 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    colorscheme = "tokyonight-moon", -- change colorscheme
+    colorscheme = "tokyonight-storm", -- change colorscheme
   },
 }
